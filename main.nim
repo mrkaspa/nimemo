9889d9fe134a33e6e6c68bdc22d4f5195cb613d3
Michel Perez <michel.ingesoft@gmail.com>
2018-04-21 13:36:18 -0500

    wip

--- main.nim
+++ main.nim
@@ -8,7 +8,12 @@ proc count(s: seq[int]): int =
 
 if isMainModule:
     for n in @[1,2,3,4]:
-        echo n
+        let str =
+            if n mod 2 == 0:
+                "even"
+            else:
+                "odd"
+        echo str
 
     echo "SUM = " & sum(100, 2).intToStr()
     echo "LEN = " & @[1, 2, 3, 4].count().intToStr()
